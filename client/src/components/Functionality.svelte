0b957e1376bb1b5a48911ae08b20c17589572de0
tatamibox <tat4mi@gmail.com>
2022-08-07 22:08:09 -0700

    create recipe portion of the ui

---
+++ client/src/components/Functionality.svelte
@@ -0,0 +1,23 @@
+<section class="app">
+    <h2 class="recipe__name">Recipes Here</h2> 
+</section>
+
+
+
+
+<style>   
+    .app {   
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 30rem;
+        height: 30rem;
+        margin: auto;
+          margin-top: 7%;
+        float: none;
+        border-radius: 50%;
+        border: 1.4rem solid #E95353;
+        background: #FF8282;
+         box-shadow: 0 0 0 1.1rem #E93737;
+    }
+</style>
--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,6 +1,8 @@
 <script>
     import Navbar from '../components/Navbar.svelte';
+    import Functionality from '../components/Functionality.svelte';
 </script>
 
 <Navbar/>
+<Functionality/>
 
