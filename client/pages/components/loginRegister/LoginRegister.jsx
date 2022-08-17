a4093b1aa523d3f0178556c86335821ced66cd96
antran1245 <antran1245@gmail.com>
2022-08-17 13:38:41 -0700

    base home screen, nav, and a default  login

---
+++ client/pages/components/loginRegister/LoginRegister.jsx
@@ -0,0 +1,11 @@
+import Nav from "../Nav";
+import Login from "./Login";
+
+export default function LoginRegister() {
+    return(
+        <>
+            <Nav/>
+            <Login/>
+        </>
+    );
+}
