53ef2ecd37a45b32b4cb6b9631d7f14b573bd990
antran1245 <antran1245@gmail.com>
2022-08-17 19:56:16 -0700

    login and register page

--- client/pages/components/loginRegister/Login.jsx
+++ client/pages/components/loginRegister/Login.jsx
@@ -1,7 +1,23 @@
-export default function Login() {
+import styles from '../../../styles/LoginRegister.module.css';
+
+export default function Login({setShow}) {
     return(
-        <div className="box">
-            Login
+        <div className={styles.box}>
+            <h2 className={styles.heading}>Login</h2>
+            <form className={styles.form}>
+                <div className={styles.formGroup}>
+                    <label className={styles.formLabel}>Username</label>
+                    <input type="text" className={styles.formInput} placeholder="Username"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.formLabel}>Password</label>
+                    <input type="text" className={styles.formInput} placeholder="Password"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <button className={styles.btn}>Login</button>
+                </div>
+            </form>
+            <p>Don't have an account? <span onClick={() => setShow('register')} className={styles.link}>Sign up</span></p>
         </div>
     );
 }
--- client/pages/components/loginRegister/LoginRegister.jsx
+++ client/pages/components/loginRegister/LoginRegister.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Nav from "../Nav";
 import Login from "./Login";
+import Register from "./Register";
 
 export default function LoginRegister() {
+    const [show, setShow] = useState('login')
     return(
         <>
             <Nav/>
-            <Login/>
+            {show === 'login' && <Login setShow={setShow}/>}
+            {show === 'register' && <Register setShow={setShow}/>}
         </>
     );
 }
