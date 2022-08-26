eaf513e60561e74f1026918b138d8033f8c58955
antran1245 <antran1245@gmail.com>
2022-08-25 21:50:45 -0700

    Demo of the arrow key switch recipes with demo data

--- client/pages/components/HomeScreen.jsx
+++ client/pages/components/HomeScreen.jsx
@@ -1,18 +1,50 @@
+import { useEffect, useState } from 'react';
 import styles from '../../styles/Home.module.css';
 
+//temporary  dataset
+const data = [
+    {title: "Recipe 1", ingredients: "Eggs, Cheese"}, 
+    {title: "Recipe 2", ingredients: "Meat, Apple"},
+    {title: "Recipe 3", ingredients: "Eggs, Meat"}
+]
 export default function HomeScreen() {
+    const [recipe, setRecipe] = useState(0)
+    useEffect(() =>  {
+        //initial recipe. should be the one from a data base
+        setRecipe(0)
+    }, [])
+
+    const switchRecipe = (direction) => {
+        if(direction === 'left') {
+            if (recipe === 0) {
+                setRecipe(data.length-1)
+            } else {
+                setRecipe(--recipe)
+            }
+        }
+        if(direction === 'right') {
+            if (recipe === data.length-1) {
+                setRecipe(0)
+            } else {
+                setRecipe(++recipe)
+            }
+        }
+    }
     return(
         <div className={styles.container}>
-            <div className={`${styles.arrow} ${styles.left} ${styles.singleArrow}`}></div>
+            <div className={`${styles.arrow} ${styles.left} ${styles.singleArrow}`} onClick={() => switchRecipe('left')}></div>
             <div className={styles.outerCircle}>
                 <div className={styles.innerCircle}>
-                    <h1 className={styles.content}>Recipe</h1>
+                    <div className={styles.content}>
+                        <h1>Title: {data[recipe].title}</h1>
+                        <p>Ingredient: {data[recipe].ingredients}</p>
+                    </div>
                 </div>
             </div>
-            <div className={`${styles.arrow} ${styles.right} ${styles.singleArrow}`}></div>
+            <div className={`${styles.arrow} ${styles.right} ${styles.singleArrow}`} onClick={() => switchRecipe('right')}></div>
             <div className={styles.arrowGroup}>
-                <div className={`${styles.arrow} ${styles.left}`}></div>
-                <div className={`${styles.arrow} ${styles.right}`}></div>
+                <div className={`${styles.arrow} ${styles.left}`} onClick={() => switchRecipe('left')}></div>
+                <div className={`${styles.arrow} ${styles.right}`} onClick={() => switchRecipe('right')}></div>
             </div>
         </div>
     );
